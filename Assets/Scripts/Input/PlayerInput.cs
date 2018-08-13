3c4a53ee9ee9bac20c1e2492f00364c931284e5c
AbsolSilver <30814163+AbsolSilver@users.noreply.github.com>
2018-08-14 09:45:56 +1000

    Initial commit

---
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+
+    public PlayerController playerCon;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+        playerCon.Move(inputH, inputV);
+    }
+}
